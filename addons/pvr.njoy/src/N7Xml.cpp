2070cefe165aa375cedfd851385f4b2d7873ff74
Lars Op den Kamp <lars@opdenkamp.eu>
2012-09-19 23:52:46 +0200

    [njoy] updated so it doesn't use curl anymore, but XBMC's CFile operations

--- addons/pvr.njoy/src/N7Xml.cpp
+++ addons/pvr.njoy/src/N7Xml.cpp
@@ -3,68 +3,21 @@
 #include "N7Xml.h"
 #include "tinyxml/tinyxml.h"
 #include "tinyxml/XMLUtils.h"
-#include <curl/curl.h>
 
 using namespace ADDON;
 
-/* partially copied from http://curl.haxx.se/libcurl/c/getinmemory.html */
-struct MemoryStruct
+bool CCurlFile::Get(const std::string &strURL, std::string &strResult)
 {
-  char *memory;
-  size_t size;
-};
-
-static size_t
-WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
-
-  mem->memory = (char*)realloc(mem->memory, mem->size + realsize + 1);
-  if (mem->memory)
+  void* fileHandle = XBMC->OpenFile(strURL.c_str(), 0);
+  if (fileHandle)
   {
-    memcpy(&(mem->memory[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->memory[mem->size] = 0;
+    char buffer[1024];
+    while (XBMC->ReadFileString(fileHandle, buffer, 1024))
+      strResult.append(buffer);
+    XBMC->CloseFile(fileHandle);
+    return true;
   }
-
-  return realsize;
-}
-
-bool CCurlFile::Get(const std::string &strURL, std::string &strResult, unsigned iTimeoutSeconds /* = 3 */)
-{
-  CURL *curl_handle;
-
-  struct MemoryStruct chunk;
-  chunk.memory = (char*)malloc(1);
-  chunk.size   = 0;
-
-  curl_global_init(CURL_GLOBAL_ALL);
-
-  curl_handle = curl_easy_init();
-  if (!curl_handle)
-    return false;
-
-  curl_easy_setopt(curl_handle, CURLOPT_URL, strURL.c_str());
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-  curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, iTimeoutSeconds);
-
-  curl_easy_perform(curl_handle);
-  curl_easy_cleanup(curl_handle);
-
-  bool bReturn(false);
-  if(chunk.memory)
-  {
-    strResult = chunk.memory;
-    free(chunk.memory);
-    bReturn = true;
-  }
-
-  curl_global_cleanup();
-
-  return bReturn;
+  return false;
 }
 
 N7Xml::N7Xml(void) :
@@ -90,7 +43,7 @@ void N7Xml::list_channels()
   CStdString strXML;
 
   CCurlFile http;
-  if(!http.Get(strUrl, strXML, 5))
+  if(!http.Get(strUrl, strXML))
   {
     XBMC->Log(LOG_DEBUG, "N7Xml - Could not open connection to N7 backend.");
   }

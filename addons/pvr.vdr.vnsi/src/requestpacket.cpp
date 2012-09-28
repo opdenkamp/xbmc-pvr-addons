b8760d0a26381e77a28b1687ed34b3b86c573283
xbmc <fernetmenta@online.de>
2012-09-28 13:32:47 +0200

    vnsi: make network interface portable

--- addons/pvr.vdr.vnsi/src/requestpacket.cpp
+++ addons/pvr.vdr.vnsi/src/requestpacket.cpp
@@ -70,10 +70,15 @@ bool cRequestPacket::init(uint32_t topcode, bool stream, bool setUserDataLength,
   serialNumber  = serialNumberCounter++;
   opcode        = topcode;
 
-  *(uint32_t*)&buffer[0] = htonl(channel);
-  *(uint32_t*)&buffer[4] = htonl(serialNumber);
-  *(uint32_t*)&buffer[8] = htonl(opcode);
-  *(uint32_t*)&buffer[userDataLenPos] = htonl(userDataLength);
+  uint32_t ul;
+  ul = htonl(channel);
+  memcpy(&buffer[0], &ul, sizeof(uint32_t));
+  ul = htonl(serialNumber);
+  memcpy(&buffer[4], &ul, sizeof(uint32_t));
+  ul = htonl(opcode);
+  memcpy(&buffer[8], &ul, sizeof(uint32_t));
+  ul = htonl(userDataLength);
+  memcpy(&buffer[userDataLenPos], &ul, sizeof(uint32_t));
   bufUsed = headerLength;
 
   return true;
@@ -85,7 +90,11 @@ bool cRequestPacket::add_String(const char* string)
   if (!checkExtend(len)) return false;
   memcpy(buffer + bufUsed, string, len);
   bufUsed += len;
-  if (!lengthSet) *(uint32_t*)&buffer[userDataLenPos] = htonl(bufUsed - headerLength);
+  if (!lengthSet)
+  {
+    uint32_t tmp = htonl(bufUsed - headerLength);
+    memcpy(&buffer[userDataLenPos], &tmp, sizeof(uint32_t));
+  }
   return true;
 }
 
@@ -94,34 +103,53 @@ bool cRequestPacket::add_U8(uint8_t c)
   if (!checkExtend(sizeof(uint8_t))) return false;
   buffer[bufUsed] = c;
   bufUsed += sizeof(uint8_t);
-  if (!lengthSet) *(uint32_t*)&buffer[userDataLenPos] = htonl(bufUsed - headerLength);
+  if (!lengthSet)
+  {
+    uint32_t tmp = htonl(bufUsed - headerLength);
+    memcpy(&buffer[userDataLenPos], &tmp, sizeof(uint32_t));
+  }
   return true;
 }
 
 bool cRequestPacket::add_S32(int32_t l)
 {
   if (!checkExtend(sizeof(int32_t))) return false;
-  *(int32_t*)&buffer[bufUsed] = htonl(l);
+  int32_t tmp = htonl(l);
+  memcpy(&buffer[bufUsed], &tmp, sizeof(int32_t));
   bufUsed += sizeof(int32_t);
-  if (!lengthSet) *(uint32_t*)&buffer[userDataLenPos] = htonl(bufUsed - headerLength);
+  if (!lengthSet)
+  {
+    uint32_t tmp = htonl(bufUsed - headerLength);
+    memcpy(&buffer[userDataLenPos], &tmp, sizeof(uint32_t));
+  }
   return true;
 }
 
 bool cRequestPacket::add_U32(uint32_t ul)
 {
   if (!checkExtend(sizeof(uint32_t))) return false;
-  *(uint32_t*)&buffer[bufUsed] = htonl(ul);
+  uint32_t tmp = htonl(ul);
+  memcpy(&buffer[bufUsed], &tmp, sizeof(uint32_t));
   bufUsed += sizeof(uint32_t);
-  if (!lengthSet) *(uint32_t*)&buffer[userDataLenPos] = htonl(bufUsed - headerLength);
+  if (!lengthSet)
+  {
+    uint32_t tmp = htonl(bufUsed - headerLength);
+    memcpy(&buffer[userDataLenPos], &tmp, sizeof(uint32_t));
+  }
   return true;
 }
 
 bool cRequestPacket::add_U64(uint64_t ull)
 {
   if (!checkExtend(sizeof(uint64_t))) return false;
-  *(uint64_t*)&buffer[bufUsed] = htonll(ull);
+  uint64_t tmp = htonll(ull);
+  memcpy(&buffer[bufUsed], &tmp, sizeof(uint64_t));
   bufUsed += sizeof(uint64_t);
-  if (!lengthSet) *(uint32_t*)&buffer[userDataLenPos] = htonl(bufUsed - headerLength);
+  if (!lengthSet)
+  {
+    uint32_t tmp = htonl(bufUsed - headerLength);
+    memcpy(&buffer[userDataLenPos], &tmp, sizeof(uint32_t));
+  }
   return true;
 }
 

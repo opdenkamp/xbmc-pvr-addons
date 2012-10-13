13d14a891392efab86418a015500c685bfdb3f28
Christian Fetzer <fetzer.ch@googlemail.com>
2012-10-13 18:21:58 +0200

    [project] Win32: Removed boost libs from downloaded build dependencies.

--- project/BuildDependencies/scripts/boost_d.bat
+++ project/BuildDependencies/scripts/boost_d.bat
@@ -8,7 +8,5 @@ CALL dlextract.bat boost %FILES%
 cd %TMP_PATH%
 
 xcopy boost_1_46_1-headers-win32\* "%CUR_PATH%\include\" /E /Q /I /Y
-xcopy boost_1_46_1-debug-libs-win32\* "%CUR_PATH%\" /E /Q /I /Y
-xcopy boost_1_46_1-libs-win32\* "%CUR_PATH%\" /E /Q /I /Y
 
 cd %LOC_PATH%

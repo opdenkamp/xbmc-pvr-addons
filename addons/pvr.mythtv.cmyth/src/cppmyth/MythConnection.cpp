080931765bef42281ef4f280d61759913794e498
janbar <jlbarriere68@gmail.com>
2013-02-12 00:22:34 +0100

    [mythtv-cmyth] Proginfo is already filled. Do not force cmyth_proginfo_fill()
    - cmyth_proginfo_fill() forces update to get details, but we have already all details. Remove this call.

--- addons/pvr.mythtv.cmyth/src/cppmyth/MythConnection.cpp
+++ addons/pvr.mythtv.cmyth/src/cppmyth/MythConnection.cpp
@@ -222,8 +222,7 @@ ProgramInfoMap MythConnection::GetRecordedPrograms()
   int len = cmyth_proglist_get_count(proglist);
   for (int i = 0; i < len; i++)
   {
-    cmyth_proginfo_t cmprog = cmyth_proglist_get_item(proglist, i);
-    MythProgramInfo prog = cmyth_proginfo_get_detail(*m_conn_t, cmprog);
+    MythProgramInfo prog = cmyth_proglist_get_item(proglist, i);
     if (!prog.IsNull()) {
       retval.insert(std::pair<CStdString, MythProgramInfo>(prog.UID().c_str(), prog));
     }
@@ -297,8 +296,7 @@ ProgramInfoMap MythConnection::GetScheduledPrograms()
   int len = cmyth_proglist_get_count(proglist);
   for (int i = 0; i < len; i++)
   {
-    cmyth_proginfo_t cmprog = cmyth_proglist_get_item(proglist, i);
-    MythProgramInfo prog = cmyth_proginfo_get_detail(*m_conn_t, cmprog);
+    MythProgramInfo prog = cmyth_proglist_get_item(proglist, i);
     if (!prog.IsNull()) {
       retval.insert(std::pair<CStdString, MythProgramInfo>(prog.UID().c_str(), prog));
     }

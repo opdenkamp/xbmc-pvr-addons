e64464faf07eccc8d5387b9071ffd31232ecc4de
xbmc <fernetmenta@online.de>
2013-03-01 13:25:03 +0100

    add version for addongui lib

--- addons/pvr.njoy/src/client.cpp
+++ addons/pvr.njoy/src/client.cpp
@@ -64,6 +64,18 @@ const char* GetMininumPVRAPIVersion(void)
   return strMinApiVersion;
 }
 
+const char* GetGUIAPIVersion(void)
+{
+  static const char *strGuiApiVersion = XBMC_GUI_API_VERSION;
+  return strGuiApiVersion;
+}
+
+const char* GetMininumGUIAPIVersion(void)
+{
+  static const char *strMinGuiApiVersion = XBMC_GUI_MIN_API_VERSION;
+  return strMinGuiApiVersion;
+}
+
 ADDON_STATUS ADDON_Create(void* hdl, void* props)
 {
   if (!props)

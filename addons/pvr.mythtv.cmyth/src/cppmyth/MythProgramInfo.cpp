opdenkamp/xbmc-pvr-addons

commit d06317d39a9c65239eb8cf909bec821c44bfd0eb

[mythtv-cmyth] Add MythProgramInfo::StorageGroup
The function returns the recording's storage group.

addons/pvr.mythtv.cmyth/src/cppmyth/MythProgramInfo.cpp
@@ -208,6 +208,11 @@ int MythProgramInfo::Priority()
   return cmyth_proginfo_priority(*m_proginfo_t); // Might want to use recpriority2 instead
 }
 
+CStdString MythProgramInfo::StorageGroup()
+{
+  return cmyth_proginfo_storagegroup(*m_proginfo_t);
+}
+
 void MythProgramInfo::SetFrameRate(const long long frameRate)
 {
   m_frameRate = frameRate;

addons/pvr.mythtv.cmyth/src/cppmyth/MythProgramInfo.h
@@ -67,6 +67,7 @@ public:
   time_t RecordingStartTime();
   time_t RecordingEndTime();
   int Priority();
+  CStdString StorageGroup();
 
   void SetFrameRate(const long long frameRate);
   long long FrameRate() const;

commit 1fd5e25551cb79ac60b18c48eb7be17d57918c6f

[mythtv-cmyth] Fix memory leak in MythProgramInfo::StorageGroup
The function cmyth_proginfo_storagegroup increases the reference counter
using ref_hold. Therefore we have to decrease it again with ref_release.

This issue was introduced in d06317d39a9c65239eb8cf909bec821c44bfd0eb.

3,520 bytes in 110 blocks are definitely lost in loss record 6 of 8
  at 0x4C2CD7B: malloc (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
  by 0x55F5FE: __ref_alloc (alloc.c:275)
  by 0x55F835: ref_strdup (alloc.c:412)
  by 0x55BB48: __cmyth_rcv_proginfo (socket.c:2235)
  by 0x55CB89: __cmyth_rcv_proglist (socket.c:2642)
  by 0x5536C8: cmyth_proglist_get_list (proglist.c:271)
  by 0x553D2C: cmyth_proglist_get_all_recorded (proglist.c:329)
  by 0x50510A: MythConnection::GetRecordedPrograms() (MythConnection.cpp:237)
  by 0x4ED1CB: PVRClientMythTV::FillRecordings() (pvrclient-mythtv.cpp:721)
  by 0x4EEF83: PVRClientMythTV::GetRecordings(ADDON_HANDLE_STRUCT*) (pvrclient-mythtv.cpp:521)

addons/pvr.mythtv.cmyth/src/cppmyth/MythProgramInfo.cpp
@@ -210,7 +210,10 @@ int MythProgramInfo::Priority()
 
 CStdString MythProgramInfo::StorageGroup()
 {
-  return cmyth_proginfo_storagegroup(*m_proginfo_t);
+  char* storageGroup = cmyth_proginfo_storagegroup(*m_proginfo_t);
+  CStdString retval(storageGroup);
+  ref_release(storageGroup);
+  return retval;
 }
 
 void MythProgramInfo::SetFrameRate(const long long frameRate)
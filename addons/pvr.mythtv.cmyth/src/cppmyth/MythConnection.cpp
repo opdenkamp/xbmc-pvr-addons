c200399d21b7e5f1f516f120ec266975346c9e61
Christian Fetzer <fetzer.ch@googlemail.com>
2013-07-27 15:47:28 +0200

    [mythtv-cmyth] Use cached hostname in GetStorageGroupFile
    The current code used GetBackendName within GetStorageGroupFile which
    triggered a QUERY_HOSTNAME call. Since GetStorageGroupFile is used to
    load all images, it is better to use the cached name / IP to avoid
    recurring QUERY_HOSTNAME calls.

--- addons/pvr.mythtv.cmyth/src/cppmyth/MythConnection.cpp
+++ addons/pvr.mythtv.cmyth/src/cppmyth/MythConnection.cpp
@@ -341,7 +341,7 @@ bool MythConnection::StopRecording(const MythProgramInfo &recording)
 MythStorageGroupFile MythConnection::GetStorageGroupFile(const CStdString &storageGroup, const CStdString &filename)
 {
   cmyth_storagegroup_file_t file = NULL;
-  CMYTH_CONN_CALL_REF(file, file == NULL, cmyth_storagegroup_get_fileinfo(*m_conn_t, const_cast<char*>(storageGroup.c_str()), const_cast<char*>(GetBackendHostname().c_str()), const_cast<char*>(filename.c_str())));
+  CMYTH_CONN_CALL_REF(file, file == NULL, cmyth_storagegroup_get_fileinfo(*m_conn_t, const_cast<char*>(storageGroup.c_str()), const_cast<char*>(GetBackendName().c_str()), const_cast<char*>(filename.c_str())));
   return MythStorageGroupFile(file);
 }
 

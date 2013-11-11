54a6b9dfbf7b0f51902f9d4c69d83baa8e47d97a
Rainer Hochecker <fernetmenta@online.de>
2013-11-11 18:44:31 +0100

    nextpvr: add missing stubs after a4550db9372fac5521f03b01fa01dd4adfebd983

--- addons/pvr.nextpvr/src/client.cpp
+++ addons/pvr.nextpvr/src/client.cpp
@@ -698,4 +698,6 @@ unsigned int GetChannelSwitchDelay(void) { return 0; }
 bool SeekTime(int,bool,double*) { return false; }
 void SetSpeed(int) {};
 time_t GetPlayingTime() { return 0; }
+time_t GetBufferTimeStart() { return 0; }
+time_t GetBufferTimeEnd() { return 0; }
 } //end extern "C"

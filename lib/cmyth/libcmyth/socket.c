opdenkamp/xbmc-pvr-addons

commit 079e53892e5979af67d4d8aca7993981a4fed200

[cmyth] Add cmyth_rcv_posmap
Adds function cmyth_rcv_posmap. The function reads the position map
from the socket.

lib/cmyth/libcmyth/socket.c
@@ -2776,6 +2776,73 @@ fail:
 	return total;
 }
 
+int
+cmyth_rcv_posmap(cmyth_conn_t conn, int *err, cmyth_posmap_t buf,
+		   int count)
+{
+	int tmp_err;
+	int consumed = 0;
+	int r;
+	int c;
+	cmyth_keyframe_t kf;
+	int i;
+
+	cmyth_dbg(CMYTH_DBG_DEBUG, "%s\n", __FUNCTION__);
+	if (!err) {
+		err = &tmp_err;
+	}
+	if (count <= 0) {
+		*err = EINVAL;
+		return 0;
+	}
+	if(!buf) {
+		*err = EINVAL;
+		cmyth_dbg(CMYTH_DBG_ERROR, "%s: NULL buffer\n", __FUNCTION__);
+		return 0;
+	}
+
+	*err = 0;
+	c = i = 0;
+
+	while (count > 0) {
+		kf = cmyth_keyframe_create();
+		if (!kf) {
+			cmyth_dbg(CMYTH_DBG_ERROR,
+				  "%s: cmyth_keyframe_create() failed\n",
+				  __FUNCTION__);
+			*err = ENOMEM;
+			break;
+		}
+		r = cmyth_rcv_keyframe(conn, err, kf, count);
+		consumed += r;
+		count -= r;
+		if (*err) {
+			ref_release(kf);
+			cmyth_dbg(CMYTH_DBG_ERROR,
+				  "%s: cmyth_rcv_keyframe() failed (%d)\n",
+				  __FUNCTION__, *err);
+			break;
+		}
+
+		if (c <= i) {
+			c += 100;
+			void *ptr = realloc(buf->posmap_list, c * sizeof(cmyth_keyframe_t));
+			if (!ptr) {
+				ref_release(kf);
+				*err = ENOMEM;
+				cmyth_dbg(CMYTH_DBG_ERROR,
+					  "%s: %s: realloc() failed for list\n",
+					  __FUNCTION__, *err);
+				break;
+			}
+			buf->posmap_list = ptr;
+		}
+		buf->posmap_list[i++] = kf;
+		buf->posmap_count = i;
+	}
+	return consumed;
+}
+
 /*
  * cmyth_rcv_freespace(cmyth_conn_t conn, cmyth_freespace_t buf, int count)
  *

lib/cmyth/libcmyth/cmyth_local.h
@@ -341,6 +341,11 @@ extern int cmyth_rcv_keyframe(cmyth_conn_t conn, int *err,
 			      cmyth_keyframe_t buf,
 			      int count);
 
+#define cmyth_rcv_posmap __cmyth_rcv_posmap
+extern int cmyth_rcv_posmap(cmyth_conn_t conn, int *err,
+			    cmyth_posmap_t buf,
+			    int count);
+
 #define cmyth_rcv_freespace __cmyth_rcv_freespace
 extern int cmyth_rcv_freespace(cmyth_conn_t conn, int *err,
 			       cmyth_freespace_t buf,

commit 1e3760afa935df5d5a9d2e911ff6a89674884228

[cmyth] Win32: Fix compilation
Commit 079e53892e5979af67d4d8aca7993981a4fed200 broke compilation.
C89 requires that all variables are declared at the beginning of a
scope block.

lib/cmyth/libcmyth/socket.c
@@ -2786,6 +2786,7 @@ cmyth_rcv_posmap(cmyth_conn_t conn, int *err, cmyth_posmap_t buf,
 	int c;
 	cmyth_keyframe_t kf;
 	int i;
+	void *ptr;
 
 	cmyth_dbg(CMYTH_DBG_DEBUG, "%s\n", __FUNCTION__);
 	if (!err) {
@@ -2826,7 +2827,7 @@ cmyth_rcv_posmap(cmyth_conn_t conn, int *err, cmyth_posmap_t buf,
 
 		if (c <= i) {
 			c += 100;
-			void *ptr = realloc(buf->posmap_list, c * sizeof(cmyth_keyframe_t));
+			ptr = realloc(buf->posmap_list, c * sizeof(cmyth_keyframe_t));
 			if (!ptr) {
 				ref_release(kf);
 				*err = ENOMEM;
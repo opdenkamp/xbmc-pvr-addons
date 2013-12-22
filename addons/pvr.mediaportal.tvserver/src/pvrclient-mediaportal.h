7d56343a5c12b9d1222c3c331d2fca38f4c39a0f
Marcel Groothuis <margro.xbmc@gmail.com>
2013-12-22 15:28:58 +0100

    [MediaPortal] Fix comment pointing to VTP thread stuff

--- addons/pvr.mediaportal.tvserver/src/pvrclient-mediaportal.h
+++ addons/pvr.mediaportal.tvserver/src/pvrclient-mediaportal.h
@@ -39,7 +39,7 @@ public:
   cPVRClientMediaPortal();
   ~cPVRClientMediaPortal();
 
-  /* VTP Listening Thread */
+  /* TVServerXBMC Listening Thread */
   static void* Process(void*);
 
   /* Server handling */

27b2f00d368e8a2853ee2c09acb5bb6e4e448487
Rainer Hochecker <fernetmenta@online.de>
2014-01-03 17:41:11 +0100

    vdr-plugin-vnsiserver: detect latm and adts by parsing

--- addons/pvr.vdr.vnsi/vdr-plugin-vnsiserver/parser.h
+++ addons/pvr.vdr.vnsi/vdr-plugin-vnsiserver/parser.h
@@ -225,6 +225,7 @@ public:
   const char *GetLanguage() { return m_language; }
   const eStreamContent Content() const { return m_streamContent; }
   const eStreamType Type() const { return m_streamType; }
+  void SetType(eStreamType type) { m_streamType = type; }
   const int GetPID() const { return m_pID; }
 
   /* Video Stream Information */

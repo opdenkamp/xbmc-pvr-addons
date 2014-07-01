c58fc87ac318d925e3fc03a72a6c137ad5f66484
Stephen Denham <stephen.denham@gmail.com>
2014-07-01 18:53:15 +0200

    Add Filmon PVR.

---
+++ addons/pvr.filmon/src/PVRFilmonData.cpp
@@ -0,0 +1,413 @@
+/*
+ *      Copyright (C) 2011 Pulse-Eight
+ *      http://www.pulse-eight.com/
+ *		Copyright (C) 2014 Stephen Denham
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "PVRFilmonData.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+using namespace ADDON;
+
+
+PVRFilmonData::PVRFilmonData(void) {
+	onLoad = true;
+}
+
+PVRFilmonData::~PVRFilmonData(void) {
+	m_channels.clear();
+	m_groups.clear();
+	m_recordings.clear();
+	m_timers.clear();
+	filmonAPIDelete();
+}
+
+bool PVRFilmonData::Load(std::string user, std::string pwd) {
+	PLATFORM::CLockObject lock(m_mutex);
+	username = user;
+	password = pwd;
+	bool res = filmonAPICreate();
+	if (res) {
+		res = filmonAPIlogin(username, password);
+		if (res) {
+			XBMC->QueueNotification(QUEUE_INFO, "Filmon user logged in");
+			lastTimeChannels = 0;
+			lastTimeGroups = 0;
+		} else {
+			XBMC->QueueNotification(QUEUE_ERROR, "Filmon user failed to login");
+		}
+	}
+	onLoad = true;
+	return res;
+}
+
+const char* PVRFilmonData::GetBackendName(void) {
+	return "Filmon API";
+}
+
+const char* PVRFilmonData::GetBackendVersion(void) {
+	return "2.2";
+}
+
+const char* PVRFilmonData:: GetConnection(void) {
+	return filmonAPIConnection().c_str();
+}
+
+void PVRFilmonData::GetDriveSpace(long long *iTotal, long long *iUsed) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "getting user storage from API");
+	filmonAPIgetUserStorage(iTotal, iUsed);
+	*iTotal = *iTotal/10;
+	*iUsed = *iUsed/10;
+	return;
+}
+
+int PVRFilmonData::GetChannelsAmount(void) {
+	unsigned int chCount = filmonAPIgetChannelCount();
+	XBMC->Log(LOG_DEBUG, "channel count is %d ", chCount);
+	return chCount;
+}
+
+PVR_ERROR PVRFilmonData::GetChannels(ADDON_HANDLE handle, bool bRadio) {
+	PLATFORM::CLockObject lock(m_mutex);
+	bool res = false;
+	bool expired = false;
+	if (time(0) - lastTimeChannels > FILMON_CACHE_TIME) {
+		XBMC->Log(LOG_DEBUG, "cache expired, getting channels from API");
+		m_channels.clear();
+		expired = true;
+	}
+
+	std::vector<unsigned int> channelList = filmonAPIgetChannels();
+	unsigned int channelCount = channelList.size();
+	unsigned int channelId = 0;
+
+	for (unsigned int i = 0; i < channelCount; i++) {
+		FILMON_CHANNEL channel;
+		channelId = channelList[i];
+		if (expired) {
+			res = filmonAPIgetChannel(channelId, &channel);
+			if (onLoad == true) {
+				XBMC->QueueNotification(QUEUE_INFO, "Filmon loaded %s",
+						channel.strChannelName.c_str());
+			}
+
+		} else {
+			for (unsigned int j = 0; j < m_channels.size(); j++) {
+				if (m_channels[j].iUniqueId == channelId) {
+					channel = m_channels[j];
+					res = true;
+					break;
+				}
+			}
+		}
+		if (res && channel.bRadio == bRadio) {
+			PVR_CHANNEL xbmcChannel;
+			memset(&xbmcChannel, 0, sizeof(PVR_CHANNEL));
+
+			xbmcChannel.iUniqueId = channel.iUniqueId;
+			xbmcChannel.bIsRadio = false;
+			xbmcChannel.iChannelNumber = channel.iChannelNumber;
+			strncpy(xbmcChannel.strChannelName, channel.strChannelName.c_str(),
+					sizeof(xbmcChannel.strChannelName) - 1);
+			strncpy(xbmcChannel.strStreamURL, channel.strStreamURL.c_str(),
+					sizeof(xbmcChannel.strStreamURL) - 1);
+			xbmcChannel.iEncryptionSystem = channel.iEncryptionSystem;
+			strncpy(xbmcChannel.strIconPath, channel.strIconPath.c_str(),
+					sizeof(xbmcChannel.strIconPath) - 1);
+			xbmcChannel.bIsHidden = false;
+			if (expired) {
+				m_channels.push_back(channel);
+			}
+			PVR->TransferChannelEntry(handle, &xbmcChannel);
+		}
+	}
+	if (lastTimeChannels == 0) {
+		XBMC->QueueNotification(QUEUE_INFO, "Filmon loaded %d channels",
+				m_channels.size());
+	}
+	if (expired) {
+		lastTimeChannels = time(0);
+	}
+	onLoad = false;
+	return PVR_ERROR_NO_ERROR;
+}
+
+int PVRFilmonData::GetChannelGroupsAmount(void) {
+	XBMC->Log(LOG_DEBUG, "getting number of groups");
+	return m_groups.size();
+}
+
+PVR_ERROR PVRFilmonData::GetChannelGroups(ADDON_HANDLE handle, bool bRadio) {
+	PLATFORM::CLockObject lock(m_mutex);
+	if (bRadio == false) {
+		if (time(0) - lastTimeGroups > FILMON_CACHE_TIME) {
+			XBMC->Log(LOG_DEBUG,
+					"cache expired, getting channel groups from API");
+			m_groups = filmonAPIgetChannelGroups();
+			lastTimeGroups = time(0);
+		}
+		for (unsigned int grpId = 0; grpId < m_groups.size(); grpId++) {
+			PVRFilmonChannelGroup group = m_groups[grpId];
+			PVR_CHANNEL_GROUP xbmcGroup;
+			memset(&xbmcGroup, 0, sizeof(PVR_CHANNEL_GROUP));
+			xbmcGroup.bIsRadio = bRadio;
+			strncpy(xbmcGroup.strGroupName, group.strGroupName.c_str(),
+					sizeof(xbmcGroup.strGroupName) - 1);
+			PVR->TransferChannelGroup(handle, &xbmcGroup);
+			XBMC->Log(LOG_DEBUG, "found group %s", xbmcGroup.strGroupName);
+		}
+	}
+	return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR PVRFilmonData::GetChannelGroupMembers(ADDON_HANDLE handle,
+		const PVR_CHANNEL_GROUP &group) {
+	PLATFORM::CLockObject lock(m_mutex);
+	if (time(0) - lastTimeGroups > FILMON_CACHE_TIME) {
+		XBMC->Log(LOG_DEBUG,
+				"cache expired, getting channel groups members from API");
+		m_groups = filmonAPIgetChannelGroups();
+		lastTimeGroups = time(0);
+	}
+	for (unsigned int grpId = 0; grpId < m_groups.size(); grpId++) {
+		PVRFilmonChannelGroup grp = m_groups.at(grpId);
+		if (strcmp(grp.strGroupName.c_str(), group.strGroupName) == 0) {
+			for (unsigned int chId = 0; chId < grp.members.size(); chId++) {
+				PVR_CHANNEL_GROUP_MEMBER xbmcGroupMember;
+				memset(&xbmcGroupMember, 0, sizeof(PVR_CHANNEL_GROUP_MEMBER));
+				strncpy(xbmcGroupMember.strGroupName, group.strGroupName,
+						sizeof(xbmcGroupMember.strGroupName) - 1);
+				xbmcGroupMember.iChannelUniqueId = grp.members[chId];
+				xbmcGroupMember.iChannelNumber = grp.members[chId];
+				XBMC->Log(LOG_DEBUG, "add member %d", grp.members[chId]);
+				PVR->TransferChannelGroupMember(handle, &xbmcGroupMember);
+			}
+			break;
+		}
+	}
+	return PVR_ERROR_NO_ERROR;
+}
+
+// Returns index into channel vector, refreshes channel entry
+int PVRFilmonData::UpdateChannel(unsigned int channelId) {
+	int index = -1;
+	XBMC->Log(LOG_DEBUG, "updating channel %d ", channelId);
+	for (unsigned int i = 0; i < m_channels.size(); i++) {
+		if (m_channels[i].iUniqueId == channelId) {
+			if (time(0) - lastTimeChannels > FILMON_CACHE_TIME) {
+				XBMC->Log(LOG_DEBUG, "cache expired, getting channel from API");
+				filmonAPIgetChannel(channelId, &m_channels[i]);
+			}
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
+
+// Called periodically to refresh EPG
+PVR_ERROR PVRFilmonData::GetEPGForChannel(ADDON_HANDLE handle,
+		const PVR_CHANNEL &channel, time_t iStart, time_t iEnd) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "getting EPG for channel");
+	unsigned int broadcastIdCount = lastTimeChannels;
+	int chIndex = PVRFilmonData::UpdateChannel(channel.iUniqueId);
+	if (chIndex >= 0) {
+		PVRFilmonChannel ch = m_channels[chIndex];
+		for (unsigned int epgId = 0; epgId < ch.epg.size(); epgId++) {
+			PVRFilmonEpgEntry &epgEntry = ch.epg.at(epgId);
+			if (epgEntry.startTime >= iStart && epgEntry.endTime <= iEnd) {
+				EPG_TAG tag;
+				memset(&tag, 0, sizeof(EPG_TAG));
+				tag.iUniqueBroadcastId = broadcastIdCount++;
+				tag.strTitle = epgEntry.strTitle.c_str();
+				tag.iChannelNumber = epgEntry.iChannelId;
+				tag.startTime = epgEntry.startTime;
+				tag.endTime = epgEntry.endTime;
+				tag.strPlotOutline = epgEntry.strPlotOutline.c_str();
+				tag.strPlot = epgEntry.strPlot.c_str();
+				tag.strIconPath = epgEntry.strIconPath.c_str();
+				tag.iGenreType = epgEntry.iGenreType;
+				tag.iGenreSubType = epgEntry.iGenreSubType;
+				tag.strGenreDescription = "";
+				tag.firstAired = 0;
+				tag.iParentalRating = 0;
+				tag.iStarRating = 0;
+				tag.bNotify = false;
+				tag.iSeriesNumber = 0;
+				tag.iEpisodeNumber = 0;
+				tag.iEpisodePartNumber = 0;
+				tag.strEpisodeName = "";
+				PVR->TransferEpgEntry(handle, &tag);
+			}
+		}
+		if (time(0) - lastTimeChannels > FILMON_CACHE_TIME) {
+			// Get PVR to re-read refreshed channels
+			if (filmonAPIlogin(username, password)) {
+				PVR->TriggerChannelGroupsUpdate();
+				PVR->TriggerChannelUpdate();
+			}
+		}
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
+
+int PVRFilmonData::GetRecordingsAmount(void) {
+	XBMC->Log(LOG_DEBUG, "getting number of recordings");
+	return m_recordings.size();
+}
+
+PVR_ERROR PVRFilmonData::GetRecordings(ADDON_HANDLE handle) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "getting recordings from API");
+	m_recordings = filmonAPIgetRecordings();
+	for (std::vector<PVRFilmonRecording>::iterator it = m_recordings.begin();
+			it != m_recordings.end(); it++) {
+		PVRFilmonRecording &recording = *it;
+		PVR_RECORDING xbmcRecording;
+
+		xbmcRecording.iDuration = recording.iDuration;
+		xbmcRecording.iGenreType = recording.iGenreType;
+		xbmcRecording.iGenreSubType = recording.iGenreSubType;
+		xbmcRecording.recordingTime = recording.recordingTime;
+
+		strncpy(xbmcRecording.strChannelName, recording.strChannelName.c_str(),
+				sizeof(xbmcRecording.strChannelName) - 1);
+		strncpy(xbmcRecording.strPlotOutline, recording.strPlotOutline.c_str(),
+				sizeof(xbmcRecording.strPlotOutline) - 1);
+		strncpy(xbmcRecording.strPlot, recording.strPlot.c_str(),
+				sizeof(xbmcRecording.strPlot) - 1);
+		strncpy(xbmcRecording.strRecordingId, recording.strRecordingId.c_str(),
+				sizeof(xbmcRecording.strRecordingId) - 1);
+		strncpy(xbmcRecording.strTitle, recording.strTitle.c_str(),
+				sizeof(xbmcRecording.strTitle) - 1);
+		strncpy(xbmcRecording.strDirectory, "Filmon",
+				sizeof(xbmcRecording.strChannelName) - 1);
+		strncpy(xbmcRecording.strStreamURL, recording.strStreamURL.c_str(),
+				sizeof(xbmcRecording.strStreamURL) - 1);
+		strncpy(xbmcRecording.strIconPath, recording.strIconPath.c_str(),
+				sizeof(xbmcRecording.strIconPath) - 1);
+		strncpy(xbmcRecording.strThumbnailPath,
+				recording.strThumbnailPath.c_str(),
+				sizeof(xbmcRecording.strThumbnailPath) - 1);
+
+		PVR->TransferRecordingEntry(handle, &xbmcRecording);
+	}
+	return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR PVRFilmonData::DeleteRecording(const PVR_RECORDING &recording) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "deleting recording %s", recording.strRecordingId);
+	if (filmonAPIdeleteRecording((unsigned int)atoi(recording.strRecordingId))) {
+		PVR->TriggerRecordingUpdate();
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
+
+int PVRFilmonData::GetTimersAmount(void) {
+	XBMC->Log(LOG_DEBUG, "getting number of timers");
+	return m_timers.size();
+}
+
+// Gets called every 5 minutes, same as Filmon session lifetime
+PVR_ERROR PVRFilmonData::GetTimers(ADDON_HANDLE handle) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "getting timers from API");
+	if (filmonAPIkeepAlive()) { // Keeps session alive
+		m_timers = filmonAPIgetTimers();
+		for (std::vector<PVRFilmonTimer>::iterator it = m_timers.begin();
+				it != m_timers.end(); it++) {
+			PVRFilmonTimer &timer = *it;
+			if ((PVR_TIMER_STATE) timer.state < PVR_TIMER_STATE_COMPLETED) {
+				PVR_TIMER xbmcTimer;
+
+				xbmcTimer.iClientIndex = timer.iClientIndex;
+				xbmcTimer.iClientChannelUid = timer.iClientChannelUid;
+				strncpy(xbmcTimer.strTitle, timer.strTitle.c_str(),
+						sizeof(xbmcTimer.strTitle) - 1);
+				strncpy(xbmcTimer.strSummary, timer.strSummary.c_str(),
+						sizeof(xbmcTimer.strSummary) - 1);
+				xbmcTimer.startTime = timer.startTime;
+				xbmcTimer.endTime = timer.endTime;
+				xbmcTimer.state = (PVR_TIMER_STATE) timer.state;
+				xbmcTimer.bIsRepeating = timer.bIsRepeating;
+				xbmcTimer.firstDay = timer.firstDay;
+				xbmcTimer.iWeekdays = timer.iWeekdays;
+				xbmcTimer.iEpgUid = timer.iEpgUid;
+				xbmcTimer.iGenreType = timer.iGenreType;
+				xbmcTimer.iGenreSubType = timer.iGenreSubType;
+				xbmcTimer.iMarginStart = timer.iMarginStart;
+				xbmcTimer.iMarginEnd = timer.iMarginEnd;
+
+				PVR->TransferTimerEntry(handle, &xbmcTimer);
+			}
+		}
+		PVR->TriggerRecordingUpdate();
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
+
+PVR_ERROR PVRFilmonData::AddTimer(const PVR_TIMER &timer) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "adding timer");
+	if (filmonAPIaddTimer(timer.iClientChannelUid, timer.startTime,
+			timer.endTime)) {
+		PVR->TriggerTimerUpdate();
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
+
+PVR_ERROR PVRFilmonData::DeleteTimer(const PVR_TIMER &timer,
+		bool bForceDelete) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "deleting timer %d", timer.iClientIndex);
+	if (filmonAPIdeleteTimer(timer.iClientIndex, bForceDelete)) {
+		PVR->TriggerTimerUpdate();
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
+
+PVR_ERROR PVRFilmonData::UpdateTimer(const PVR_TIMER& timer) {
+	PLATFORM::CLockObject lock(m_mutex);
+	XBMC->Log(LOG_DEBUG, "updating timer");
+	if (filmonAPIdeleteTimer(timer.iClientIndex, true)
+			&& filmonAPIaddTimer(timer.iClientChannelUid, timer.startTime,
+					timer.endTime)) {
+		PVR->TriggerTimerUpdate();
+		return PVR_ERROR_NO_ERROR;
+	} else {
+		return PVR_ERROR_SERVER_ERROR;
+	}
+}
---
+++ addons/pvr.filmon/src/client.cpp
@@ -0,0 +1,474 @@
+/*
+ *      Copyright (C) 2011 Pulse-Eight
+ *      http://www.pulse-eight.com/
+ *		Copyright (C) 2014 Stephen Denham
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "client.h"
+
+#include "platform/util/util.h"
+#include "PVRFilmonData.h"
+#include "xbmc_pvr_dll.h"
+
+using namespace std;
+using namespace ADDON;
+
+#ifdef TARGET_WINDOWS
+#define snprintf _snprintf
+#endif
+
+bool m_bCreated = false;
+ADDON_STATUS m_CurStatus = ADDON_STATUS_UNKNOWN;
+PVRFilmonData *m_data = NULL;
+bool m_bIsPlaying = false;
+PVRFilmonChannel m_currentChannel;
+
+/* User adjustable settings are saved here.
+ * Default values are defined inside client.h
+ * and exported to the other source files.
+ */
+std::string g_strUserPath = "";
+std::string g_strClientPath = "";
+std::string g_strUsername = "";
+std::string g_strPassword = "";
+
+CHelper_libXBMC_addon *XBMC = NULL;
+CHelper_libXBMC_pvr *PVR = NULL;
+
+extern "C" {
+
+void ADDON_ReadSettings(void) {
+	char buffer[1024] = "";
+
+	/* read setting "username" from settings.xml */
+	if (XBMC->GetSetting("username", buffer))
+		g_strUsername = buffer;
+	else
+		g_strUsername = "";
+	buffer[0] = 0; /* Set the end of string */
+
+	/* read setting "password" from settings.xml */
+	if (XBMC->GetSetting("password", buffer))
+		g_strPassword = buffer;
+	else
+		g_strPassword = "";
+
+	XBMC->Log(LOG_DEBUG, "%s - read PVR Filmon settings", __FUNCTION__);
+}
+
+ADDON_STATUS ADDON_Create(void* hdl, void* props) {
+	if (!hdl || !props)
+		return ADDON_STATUS_UNKNOWN;
+
+	PVR_PROPERTIES* pvrprops = (PVR_PROPERTIES*) props;
+
+	XBMC = new CHelper_libXBMC_addon;
+	if (!XBMC->RegisterMe(hdl)) {
+		SAFE_DELETE(XBMC);
+		return ADDON_STATUS_PERMANENT_FAILURE;
+	}
+
+	PVR = new CHelper_libXBMC_pvr;
+	if (!PVR->RegisterMe(hdl)) {
+		SAFE_DELETE(PVR);
+		SAFE_DELETE(XBMC);
+		return ADDON_STATUS_PERMANENT_FAILURE;
+	}
+
+	XBMC->Log(LOG_DEBUG, "%s - Creating the PVR Filmon add-on", __FUNCTION__);
+
+	m_CurStatus = ADDON_STATUS_UNKNOWN;
+	g_strUserPath = pvrprops->strUserPath;
+	g_strClientPath = pvrprops->strClientPath;
+
+	ADDON_ReadSettings();
+
+	m_data = new PVRFilmonData;
+	if (m_data->Load(g_strUsername, g_strPassword)) {
+		m_CurStatus = ADDON_STATUS_OK;
+		m_bCreated = true;
+		XBMC->Log(LOG_DEBUG, "%s - Created the PVR Filmon add-on",
+				__FUNCTION__);
+	} else {
+		XBMC->Log(LOG_ERROR, "%s - Failed to connect to Filmon, check settings",
+				__FUNCTION__);
+		m_CurStatus = ADDON_STATUS_LOST_CONNECTION;
+	}
+	return m_CurStatus;
+}
+
+ADDON_STATUS ADDON_GetStatus() {
+	return m_CurStatus;
+}
+
+void ADDON_Destroy() {
+	delete m_data;
+	m_bCreated = false;
+	m_CurStatus = ADDON_STATUS_UNKNOWN;
+}
+
+bool ADDON_HasSettings() {
+	return true;
+}
+
+unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet) {
+	return 0;
+}
+
+ADDON_STATUS ADDON_SetSetting(const char *settingName,
+		const void *settingValue) {
+	string str = settingName;
+	if (str == "username") {
+		string tmp_sUsername = g_strUsername;
+		g_strUsername = (const char*) settingValue;
+		if (tmp_sUsername != g_strUsername) {
+			XBMC->Log(LOG_INFO, "%s - Changed Setting 'username'",
+					__FUNCTION__);
+			return ADDON_STATUS_NEED_RESTART;
+		}
+	} else if (str == "password") {
+		string tmp_sPassword = g_strPassword;
+		g_strPassword = (const char*) settingValue;
+		if (tmp_sPassword != g_strPassword) {
+			XBMC->Log(LOG_INFO, "%s - Changed Setting 'password'",
+					__FUNCTION__);
+			return ADDON_STATUS_NEED_RESTART;
+		}
+	}
+	return ADDON_STATUS_OK;
+}
+
+void ADDON_Stop() {
+}
+
+void ADDON_FreeSettings() {
+}
+
+void ADDON_Announce(const char *flag, const char *sender, const char *message,
+		const void *data) {
+}
+
+/***********************************************************
+ * PVR Client AddOn specific public library functions
+ ***********************************************************/
+
+const char* GetPVRAPIVersion(void) {
+	static const char *strApiVersion = XBMC_PVR_API_VERSION;
+	return strApiVersion;
+}
+
+const char* GetMininumPVRAPIVersion(void) {
+	static const char *strMinApiVersion = XBMC_PVR_MIN_API_VERSION;
+	return strMinApiVersion;
+}
+
+const char* GetGUIAPIVersion(void) {
+	static const char *strGuiApiVersion = XBMC_GUI_API_VERSION;
+	return strGuiApiVersion;
+}
+
+const char* GetMininumGUIAPIVersion(void) {
+	static const char *strMinGuiApiVersion = XBMC_GUI_MIN_API_VERSION;
+	return strMinGuiApiVersion;
+}
+
+PVR_ERROR GetAddonCapabilities(PVR_ADDON_CAPABILITIES* pCapabilities) {
+	pCapabilities->bSupportsTV = true;
+	pCapabilities->bSupportsEPG = true;
+	pCapabilities->bSupportsRecordings = true;
+	pCapabilities->bSupportsTimers = true;
+	pCapabilities->bSupportsChannelGroups = true;
+	pCapabilities->bSupportsRadio = false;
+	pCapabilities->bSupportsRecordingFolders = false;
+	pCapabilities->bHandlesInputStream = false;
+	pCapabilities->bHandlesDemuxing = false;
+	pCapabilities->bSupportsChannelScan = false;
+	pCapabilities->bSupportsLastPlayedPosition = false;
+	pCapabilities->bSupportsRecordingEdl = false;
+
+	XBMC->Log(LOG_DEBUG, "%s - got PVR Filmon capabilities", __FUNCTION__);
+
+	return PVR_ERROR_NO_ERROR;
+}
+
+const char *GetBackendName(void) {
+	static const char *strBackendName = m_data->GetBackendName();
+	XBMC->Log(LOG_DEBUG, "%s - got PVR Filmon backend name; %s", __FUNCTION__,
+			strBackendName);
+	return strBackendName;
+}
+
+const char *GetBackendVersion(void) {
+	static CStdString strBackendVersion = m_data->GetBackendVersion();
+	XBMC->Log(LOG_DEBUG, "%s - got PVR Filmon backend version; %s",
+			__FUNCTION__, strBackendVersion.c_str());
+	return strBackendVersion.c_str();
+}
+
+const char *GetConnectionString(void) {
+	static CStdString strConnectionString = m_data->GetConnection();
+	return strConnectionString.c_str();
+}
+
+PVR_ERROR GetDriveSpace(long long *iTotal, long long *iUsed) {
+	m_data->GetDriveSpace(iTotal, iUsed);
+	return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR GetEPGForChannel(ADDON_HANDLE handle, const PVR_CHANNEL &channel,
+		time_t iStart, time_t iEnd) {
+	if (m_data) {
+		XBMC->Log(LOG_DEBUG, "%s - getting PVR Filmon EPG", __FUNCTION__);
+		return m_data->GetEPGForChannel(handle, channel, iStart, iEnd);
+	}
+	XBMC->Log(LOG_DEBUG, "%s - failed getting PVR Filmon EPG", __FUNCTION__);
+	return PVR_ERROR_SERVER_ERROR;
+}
+
+int GetChannelsAmount(void) {
+	if (m_data)
+		return m_data->GetChannelsAmount();
+
+	return -1;
+}
+
+PVR_ERROR GetChannels(ADDON_HANDLE handle, bool bRadio) {
+	if (m_data) {
+		XBMC->Log(LOG_DEBUG, "%s - getting PVR Filmon channels", __FUNCTION__);
+		return m_data->GetChannels(handle, bRadio);
+	}
+	XBMC->Log(LOG_ERROR, "%s - failed getting PVR Filmon channels",
+			__FUNCTION__);
+	return PVR_ERROR_SERVER_ERROR;
+}
+
+int GetChannelGroupsAmount(void) {
+	if (m_data)
+		return m_data->GetChannelGroupsAmount();
+
+	return -1;
+}
+
+PVR_ERROR GetChannelGroups(ADDON_HANDLE handle, bool bRadio) {
+	if (m_data)
+		return m_data->GetChannelGroups(handle, bRadio);
+
+	return PVR_ERROR_SERVER_ERROR;
+}
+
+PVR_ERROR GetChannelGroupMembers(ADDON_HANDLE handle,
+		const PVR_CHANNEL_GROUP &group) {
+	if (m_data)
+		return m_data->GetChannelGroupMembers(handle, group);
+
+	return PVR_ERROR_SERVER_ERROR;
+}
+
+PVR_ERROR SignalStatus(PVR_SIGNAL_STATUS &signalStatus) {
+	snprintf(signalStatus.strAdapterName, sizeof(signalStatus.strAdapterName),
+			m_data->GetBackendName());
+	snprintf(signalStatus.strAdapterStatus,
+			sizeof(signalStatus.strAdapterStatus), "OK");
+
+	return PVR_ERROR_NO_ERROR;
+}
+
+int GetRecordingsAmount(void) {
+	if (m_data)
+		return m_data->GetRecordingsAmount();
+
+	return -1;
+}
+
+PVR_ERROR GetRecordings(ADDON_HANDLE handle) {
+	if (m_data)
+		return m_data->GetRecordings(handle);
+
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+
+PVR_ERROR DeleteRecording(const PVR_RECORDING &recording) {
+	if (!m_data)
+		return PVR_ERROR_SERVER_ERROR;
+
+	return m_data->DeleteRecording(recording);
+}
+
+int GetTimersAmount(void) {
+	if (!m_data)
+		return 0;
+
+	return m_data->GetTimersAmount();
+}
+
+PVR_ERROR GetTimers(ADDON_HANDLE handle) {
+	if (!m_data)
+		return PVR_ERROR_SERVER_ERROR;
+
+	return m_data->GetTimers(handle);
+}
+
+PVR_ERROR AddTimer(const PVR_TIMER &timer) {
+	if (!m_data)
+		return PVR_ERROR_SERVER_ERROR;
+
+	return m_data->AddTimer(timer);
+}
+
+PVR_ERROR DeleteTimer(const PVR_TIMER &timer, bool bForceDelete) {
+	if (!m_data)
+		return PVR_ERROR_SERVER_ERROR;
+
+	return m_data->DeleteTimer(timer, bForceDelete);
+
+}
+
+PVR_ERROR UpdateTimer(const PVR_TIMER& timer) {
+	if (!m_data)
+		return PVR_ERROR_SERVER_ERROR;
+
+	return m_data->UpdateTimer(timer);
+}
+
+/** UNUSED API FUNCTIONS */
+PVR_ERROR DialogChannelScan(void) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR CallMenuHook(const PVR_MENUHOOK &menuhook,
+		const PVR_MENUHOOK_DATA &item) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR DeleteChannel(const PVR_CHANNEL &channel) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR RenameChannel(const PVR_CHANNEL &channel) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR MoveChannel(const PVR_CHANNEL &channel) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+bool SwitchChannel(const PVR_CHANNEL& channel) {
+	return false;
+}
+int GetCurrentClientChannel(void) {
+	return 0;
+}
+PVR_ERROR DialogChannelSettings(const PVR_CHANNEL &channel) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR DialogAddChannel(const PVR_CHANNEL &channel) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+bool OpenRecordedStream(const PVR_RECORDING &recording) {
+	return false;
+}
+bool OpenLiveStream(const PVR_CHANNEL& channel) {
+	return false;
+}
+const char* GetLiveStreamURL(const PVR_CHANNEL& channel) {
+	return "";
+}
+void CloseLiveStream(void) {
+}
+void CloseRecordedStream(void) {
+}
+int ReadRecordedStream(unsigned char *pBuffer, unsigned int iBufferSize) {
+	return 0;
+}
+long long SeekRecordedStream(long long iPosition,
+		int iWhence /* = SEEK_SET */) {
+	return 0;
+}
+long long PositionRecordedStream(void) {
+	return -1;
+}
+long long LengthRecordedStream(void) {
+	return 0;
+}
+PVR_ERROR GetStreamProperties(PVR_STREAM_PROPERTIES* pProperties) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+void DemuxReset(void) {
+}
+void DemuxFlush(void) {
+}
+int ReadLiveStream(unsigned char *pBuffer, unsigned int iBufferSize) {
+	return 0;
+}
+long long SeekLiveStream(long long iPosition, int iWhence /* = SEEK_SET */) {
+	return -1;
+}
+long long PositionLiveStream(void) {
+	return -1;
+}
+long long LengthLiveStream(void) {
+	return -1;
+}
+PVR_ERROR RenameRecording(const PVR_RECORDING &recording) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR SetRecordingPlayCount(const PVR_RECORDING &recording, int count) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+PVR_ERROR SetRecordingLastPlayedPosition(const PVR_RECORDING &recording,
+		int lastplayedposition) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+int GetRecordingLastPlayedPosition(const PVR_RECORDING &recording) {
+	return -1;
+}
+PVR_ERROR GetRecordingEdl(const PVR_RECORDING&, PVR_EDL_ENTRY[], int*) {
+	return PVR_ERROR_NOT_IMPLEMENTED;
+}
+void DemuxAbort(void) {
+}
+DemuxPacket* DemuxRead(void) {
+	return NULL;
+}
+unsigned int GetChannelSwitchDelay(void) {
+	return 0;
+}
+void PauseStream(bool bPaused) {
+}
+bool CanPauseStream(void) {
+	return true;
+}
+bool CanSeekStream(void) {
+	return true;
+}
+bool SeekTime(int time, bool backwards, double *startpts) {
+	if (backwards) {
+		return true;
+	} else {
+		return false;
+	}
+}
+void SetSpeed(int) {
+}
+;
+time_t GetPlayingTime() {
+	return 0;
+}
+time_t GetBufferTimeStart() {
+	return 0;
+}
+time_t GetBufferTimeEnd() {
+	return 0;
+}
+}

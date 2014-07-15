3abdf854de788a7cb4f57d8ddaf0021633085a5a
Ryan Gribble <ryan.gribble@gmail.com>
2014-07-15 14:32:12 +1000

    bump version to 3.99

--- addons/pvr.wmc/src/clientversion.h
+++ addons/pvr.wmc/src/clientversion.h
@@ -23,5 +23,5 @@
 
 inline CStdString PVRWMC_GetClientVersion()
 {
-	return "0.3.98";	// ALSO CHANGE IN REV NUMBER in 'addon.xml.in' 
+	return "0.3.99";	// ALSO CHANGE IN REV NUMBER in 'addon.xml.in' 
 }
